b686abe19d339720ae78388f61dee54958d5f35e
gavin <gavinklfong@gmail.com>
2025-04-01 23:57:28 +0100

    rename endpoint to controller

--- router/router.go
+++ router/router.go
@@ -1,38 +1,38 @@
 package router
 
 import (
-	"github.com/gavinklfong/go-rest-api-demo/endpoint"
+	"github.com/gavinklfong/go-rest-api-demo/controller"
 	"github.com/gin-gonic/gin"
 )
 
 type Router struct {
-	e                 *gin.Engine
-	getRateEndpoint   *endpoint.GetRateEndpoint
-	bookRateEndpoint  *endpoint.BookRateEndpoint
-	tradeDealEndpoint *endpoint.TradeDealEndpoint
+	e                   *gin.Engine
+	getRateController   *controller.GetRateController
+	bookRateController  *controller.BookRateController
+	tradeDealController *controller.TradeDealController
 }
 
-func NewRouter(getRateEndpoint *endpoint.GetRateEndpoint,
-	bookRateEndpoint *endpoint.BookRateEndpoint,
-	tradeDealEndpoint *endpoint.TradeDealEndpoint) *Router {
+func NewRouter(getRateController *controller.GetRateController,
+	bookRateController *controller.BookRateController,
+	tradeDealController *controller.TradeDealController) *Router {
 	r := &Router{e: gin.Default(),
-		getRateEndpoint:   getRateEndpoint,
-		bookRateEndpoint:  bookRateEndpoint,
-		tradeDealEndpoint: tradeDealEndpoint}
+		getRateController:   getRateController,
+		bookRateController:  bookRateController,
+		tradeDealController: tradeDealController}
 	r.doSetup()
 	return r
 }
 
 func (r *Router) doSetup() {
 
-	r.e.GET("/rates/:baseCurrency/:counterCurrency", r.getRateEndpoint.GetRateByCurrencyPair)
-	r.e.GET("/rates/:baseCurrency", r.getRateEndpoint.GetRateByBaseCurrency)
-	r.e.GET("/rates/latest", r.getRateEndpoint.GetAllRates)
+	r.e.GET("/rates/:baseCurrency/:counterCurrency", r.getRateController.GetRateByCurrencyPair)
+	r.e.GET("/rates/:baseCurrency", r.getRateController.GetRateByBaseCurrency)
+	r.e.GET("/rates/latest", r.getRateController.GetAllRates)
 
-	r.e.POST("/rates/book", r.bookRateEndpoint.BookRate)
+	r.e.POST("/rates/book", r.bookRateController.BookRate)
 
-	r.e.GET("/deals", r.tradeDealEndpoint.GetTradeDeal)
-	r.e.POST("/deals", r.tradeDealEndpoint.SubmitTradeDeal)
+	r.e.GET("/deals", r.tradeDealController.GetTradeDeal)
+	r.e.POST("/deals", r.tradeDealController.SubmitTradeDeal)
 }
 
 func (r *Router) Run(s string) {
